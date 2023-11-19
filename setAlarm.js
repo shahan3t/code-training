12bc538106f2bef3bdb26f615d5abec7f7bbca45
Shahan Tariq <shahan3t@outlook.com>
2023-11-19 22:18:06 +0100

    Create setAlarm.js

---
+++ setAlarm.js
@@ -0,0 +1,4 @@
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise.
+function setAlarm(employed, vacation){
+  return employed && !vacation
+}
