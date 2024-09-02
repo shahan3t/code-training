63cb1d01b9df96659ee03a7324e097b793569bf1
Shahan Tariq <shahan3t@outlook.com>
2024-09-02 11:18:06 +0200

    Create allStarCode.js

---
+++ allStarCode.js
@@ -0,0 +1,11 @@
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+function strCount(str, letter){  
+    return str.split("").filter((char) => char === letter).length
+  }
+//CW solution below
+  function strCount2(str, letter){  
+    return str.split(letter).length-1
+  }
