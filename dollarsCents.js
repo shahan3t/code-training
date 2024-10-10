304f85d0af4f3f3e9821d51e0ef0462d125abc23
Shahan Tariq <shahan3t@outlook.com>
2024-10-10 11:53:21 +0200

    Create dollarsCents.js

---
+++ dollarsCents.js
@@ -0,0 +1,5 @@
+// The company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents.
+// 39.99 becomes $39.99
+function formatMoney(amount){
+    return '$' + amount.toFixed(2);
+  }
