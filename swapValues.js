a6f942c8159f69eb2dc9795fe6d092c847de5050
Shahan Tariq <shahan3t@outlook.com>
2024-11-07 22:49:07 +0100

    Create swapValues.js

---
+++ swapValues.js
@@ -0,0 +1,10 @@
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+function swapValues(args) {
+    let temp = args[0];
+    args[0] = args[1];
+    args[1] = temp;
+  return args
+}
+function swapValues(args) {
+    return args.reverse();
+  }
